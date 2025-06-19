9b0504e39d64314cc3856d09be0f6c1f48f87409
laziz577 <temirovabdulaziz042@gmail.com>
2025-06-19 09:31:19 +0500

    bajarildi

--- if03.py
+++ if03.py
@@ -1,1 +1,13 @@
+number = int(input("Kirish:"))
 
+print("Chiqish:")
+
+if number % 2 == 0:
+    print(f"{number} soni 2 ga bo'linadi")
+
+if number % 3 == 0:
+    print(f"{number} soni 3 ga bo'linadi")
+
+if number % 5 == 0:
+    print(f"{number} soni 5 ga bo'linadi")
+    
