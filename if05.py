28560ba9d606d665426dc6a6d034e3444493d659
laziz577 <temirovabdulaziz042@gmail.com>
2025-06-19 09:58:27 +0500

    bajarildi

--- if05.py
+++ if05.py
@@ -1,1 +1,12 @@
+temp = float(input("Harorat kiriting (C):"))
 
+if temp <= 0:
+    print("Juda sovuq! Issiq kiyim kiying")
+
+
+if temp >= 0 and temp <= 14:
+    print("Sovuq.Kurtka kiying")
+
+if temp >= 15:
+    print("Ob-havo yahshi")
+            
