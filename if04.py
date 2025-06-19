1874af7fe7fffecff27f69bda3ceefbe12de6043
laziz577 <temirovabdulaziz042@gmail.com>
2025-06-19 09:52:13 +0500

    bajarildi

--- if04.py
+++ if04.py
@@ -1,1 +1,15 @@
+chipta = 100
 
+yosh = int(input("Yoshingizni kiriting:"))
+
+if yosh < 7:
+    chipta *= 0.5
+
+if yosh >= 7 and yosh <= 17:
+    chipta *= 0.8
+
+if yosh > 60 :
+    chipta *= 0.7
+
+print("chipta narxi:" , chipta)
+    
