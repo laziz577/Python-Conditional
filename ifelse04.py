764c9b82bc40498af43c75f804c57dc3c9342070
laziz577 <temirovabdulaziz042@gmail.com>
2025-06-19 10:19:38 +0500

    bajarildi

--- ifelse04.py
+++ ifelse04.py
@@ -1,1 +1,13 @@
+balans = 500_000
+amount = float(input("Qancha pul yechmoqchisiz:"))
+
+if amount >= 0:
+    if amount <= balans:
+        print("Pul yechildi.Qolgan balans x so'm")
+       
+    else:
+     print(f"Mablag' yetarli emas.Sizning balansingiz: {balans} so'm.")
+
+else:
+   print("Manfiy summa kiritib bo'lmaydi.")
 
